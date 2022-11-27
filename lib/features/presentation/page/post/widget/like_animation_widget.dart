ffb64877bbed1cc87cd2bc52ad9125a7cee4cd38
AdnanKhan45 <ak187429@gmail.com>
2022-11-27 12:47:40 -0700

    post 04

---
+++ lib/features/presentation/page/post/widget/like_animation_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LikeAnimationWidget extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  final bool isLikeAnimating;
+  final VoidCallback? onLikeFinish;
+
+  const LikeAnimationWidget(
+      {Key? key,
+      required this.child,
+      required this.duration,
+      required this.isLikeAnimating,
+      this.onLikeFinish})
+      : super(key: key);
+
+  @override
+  State<LikeAnimationWidget> createState() => _LikeAnimationWidgetState();
+}
+
+class _LikeAnimationWidgetState extends State<LikeAnimationWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> scale;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        vsync: this, duration: Duration(milliseconds: widget.duration.inMilliseconds));
+    scale = Tween<double>(begin: 1, end: 1.2).animate(_controller);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant LikeAnimationWidget oldWidget) {
+    if (widget.isLikeAnimating != oldWidget.isLikeAnimating) {
+      beginLikeAnimation();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  beginLikeAnimation() async {
+    if (widget.isLikeAnimating) {
+      await _controller.forward();
+      await _controller.reverse();
+      await Future.delayed(Duration(milliseconds: 200));
+      if (widget.onLikeFinish != null) {
+        widget.onLikeFinish!();
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: scale,
+      child: widget.child,
+    );
+  }
+}
